d1fde08565d4f234aab8e94989cdd60e6dd83163
Andrew Lane <contact@lungers.com>
2021-05-16 02:23:41 -0400

    Reverse numbers for the roller

--- Roller.vue
+++ Roller.vue
@@ -45,7 +45,7 @@ export default {
         return {
             isRollStart: false,
             isAnimationEnd: false,
-            charList: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+            charList: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'],
         };
     },
 
